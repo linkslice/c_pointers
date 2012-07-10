4e19885134e7751ac4a2094d653fe2d3aeb4cafa
linkslice <sparctacus@gmail.com>
2012-07-09 23:17:38 -0700

    added first exercise

---
+++ ptrtut10.c
@@ -0,0 +1,22 @@
+/* program 1.1 from c pointers and arrays tutorial */
+
+#include <stdio.h>
+
+int j, k;
+int *ptr;
+
+int main(void)
+{
+
+	j = 1;
+	k = 2;
+	ptr = &k;
+	printf("\n");
+	printf("j has the value %d and is stored at %p\n", j, (void *)&j);
+	printf("k has the value %d and is stored at %p\n", k, (void *)&k);
+	printf("ptr has the value %d and is stored at %p\n", ptr, (void *)&ptr);
+	printf("The value of the integer pointer to by ptr is %d\n", *ptr);
+
+	return 0;
+
+}
