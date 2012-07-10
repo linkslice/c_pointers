fae380e77a1640438f89bcf1a20af091e1cd7843
linkslice <sparctacus@gmail.com>
2012-07-09 23:24:22 -0700

    added exercise 2.1

---
+++ program2-1.c
@@ -0,0 +1,22 @@
+/* prigram 2.1 */
+
+#include <stdio.h>
+
+int my_array[] = { 1,23,17,4,-5,100};
+int *ptr;
+
+int main(void)
+{
+
+	int i;
+	ptr = &my_array[0];
+	
+	printf("\n\n");
+	for (i=0; i<6; i++)
+	{
+		printf("my_array[%d] = %d   ", my_array[i]); /* <-- A */
+		printf("ptr + %d = %d\n", i, *(ptr + i));    /* <-- B */
+		
+	}
+	return 0;
+}
